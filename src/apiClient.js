0cac2fe4b2f2e2f76727a3e89fe0b8e073486962
ellie <ellieyhc@gmail.com>
2019-08-21 19:23:32 -0700

    Modularize api calls

---
+++ src/apiClient.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+const BASE_URI = 'http://localhost:8080/api'
+
+const client = axios.create({
+    baseURL: BASE_URI,
+});
+
+class APIClient {
+    // constructor(accessToken) {
+    //     this.accessToken = accessToken;
+    // }
+   
+    getOnePlan(planId) {
+        return this.perform('get', `/plan/${planId}`)
+    }
+
+    savePlan(planId, plan) {
+        return this.perform('post', `/plan/${planId}/update`, plan)
+    }
+
+    async perform (method, resource, data) {
+        return client({
+            method,
+            url: resource,
+            data,
+        }).then(res => {
+            return res.data ? res.data : {};
+        }).catch(e => {
+            console.log(e)
+        })
+    }
+}
+
+export default APIClient;
