1441df1fe3df63b7a8d367a251a34c5e4a1b9cb9
ellie <ellieyhc@gmail.com>
2019-09-14 22:27:13 -0700

    Format key

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ module.exports = (() => {
 				cert: {
 					projectId: process.env.FIREBASE_PROJECT_ID,
 					clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-					privateKey: process.env.FIREBASE_PRIVATE_KEY
+					privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 				}
 			},
 			// Server settings
